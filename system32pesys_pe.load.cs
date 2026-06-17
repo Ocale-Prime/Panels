d9282f7457def3ab8c57b2eb59a857039400cd47
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-17 17:18:57 +0200

    Add system32pesys_pe.load.cs with initialization logic

---
+++ system32pesys_pe.load.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Formats.Asn1;
+
+namespace system
+{
+    class local_init
+    {
+        protected static void local()
+        {
+            var system_load = new function();
+            system_load.init_enterprises = sizeof(system_load.funct_cpu);
+        }
+    }
+
+  class return_init : local_init
+  {
+      protected static void ld()
+      {
+          Console.WriteLine("cpup")
+      }
+  }
+}
