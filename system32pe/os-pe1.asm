9d6031c89f0d0e52b82e28f9ae16f8bcdc8d3d4e
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-18 17:29:40 +0200

    Enhance os-pe1.asm with comments and new sections
    Updated comments and added assembly code sections for MainData and StarterData.

---
+++ system32pe/os-pe1.asm
@@ -0,0 +1,11 @@
+; (C) Ocale - Panels Operating system, Create the os-pe tool, this is the phase for the tools and all of the, this will be fun. This is only for some CPu installation and frivers, and all of that stuff. [this Software is protected by the Ocale Corporation Policies. learn more]
+
+.section MainData:
+  mov rdx, 2
+  cmp dx, 3
+  int 0x43002f
+
+.section StarterData:
+  mov dx, 3
+  cmp dx, 2
+  ret 0
