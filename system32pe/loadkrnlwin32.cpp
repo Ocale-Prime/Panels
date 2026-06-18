fcdfe3de29d5da0661ec228c531a340a1995584e
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-18 20:52:38 +0200

    Add create_memory function in loadkrnlwin32.cpp + vmware iso integration krnl loader

---
+++ system32pe/loadkrnlwin32.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+using namespace std;
+
+namespace cr1
+{
+    template<typename Toy>
+    static template<typename joid>
+    void create_memory(joid loader)
+    {
+        LOCALVMWARE vmware_tools;
+        vmware_tools.isofunction = sizeof(vmware_graphics.vmloader.graphics::loader());
+        loader.tools = vmware_tools.load_iso();
+    };
+
+  return 0;
+};
